0c553a72dba28bda5dd519a50771ee4ed6e5157d
Pete Houston <contact@petehouston.com>
2019-01-10 21:04:54 +0700

    Update dashboard UI

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -10,7 +10,54 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Awesome Dashboard is coming!'),
+        title: Text('Dashboard'),
+        elevation: .1,
+        backgroundColor: Color.fromRGBO(49, 87, 110, 1.0),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 2.0),
+        child: GridView.count(crossAxisCount: 2, padding: EdgeInsets.all(3.0), children: <Widget>[
+          makeDashboardItem('Ordbog', Icons.book),
+          makeDashboardItem('Ordbog', Icons.book),
+          makeDashboardItem('Ordbog', Icons.book),
+          makeDashboardItem('Ordbog', Icons.book),
+          makeDashboardItem('Ordbog', Icons.book),
+          makeDashboardItem('Ordbog', Icons.book),
+        ],),
+      ),
+    );
+  }
+
+  Card makeDashboardItem(String title, IconData icon) {
+    return Card(
+      elevation: 1.0,
+      margin: EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(220, 220, 220, 1.0),
+        ),
+        child: InkWell(
+          onTap: () {},
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            verticalDirection: VerticalDirection.down,
+            children: <Widget>[
+              SizedBox(height: 50.0,),
+              Center(
+                child: Icon(
+                  icon,
+                  size: 40.0,
+                    color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              Center(
+                child: Text(title, style: TextStyle(fontSize: 18.0, color: Colors.black),),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
